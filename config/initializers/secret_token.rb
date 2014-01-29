85f09a4280f821dc8e39d8b5cfd7ec0bec9e1885
Vincent Chu <c.tnecniv@gmail.com>
2014-01-28 20:37:42 -0800

    first version of new site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newla::Application.config.secret_key_base = '7113618c599b3dcc31b7539c1371f78065a08cafb9e528209b87a7ee95cfb8ed2c41c89ea9bf5abdde6b12fd09dcad385dbd7714e30a777918bfd43de6071fd6'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Newla::Application.routes.draw do
+  root "home#index"
+end
